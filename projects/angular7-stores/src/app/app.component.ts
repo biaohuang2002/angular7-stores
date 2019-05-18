beb554e51602e834b7b038a98afa2896ba5354c9
Biao Huang <biaohuang2002@gmail.com>
2019-05-19 01:18:40 +0200

    ng add @angular/material

--- projects/angular7-stores/src/app/app.component.ts
+++ projects/angular7-stores/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private _unsubscribe$ = new Subject();
   title = 'angular7-stores';
+  
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+    .observe(Object.values(Breakpoints)).pipe(
+      takeUntil( this._unsubscribe$ )
+    ).subscribe(result => {
+      if (result.matches) {
+        this.activateHandsetLayout(result);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this._unsubscribe$.next();
+    this._unsubscribe$.complete();
+  }
+
+  activateHandsetLayout(result) {
+    console.log(Breakpoints, result);
+  }
 }
