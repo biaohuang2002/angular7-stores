4fa36ca90aa7ed2e9bbf8cccda8dd2e445df67d8
Biao Huang <biaohuang2002@gmail.com>
2019-05-19 16:02:26 +0200

    breakpoints

--- projects/angular7-stores/src/app/breakpoint.service.ts
+++ projects/angular7-stores/src/app/breakpoint.service.ts
@@ -3,33 +3,56 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Platform } from '@angular/cdk/platform';
 
-@Injectable({
+@Injectable( {
   providedIn: 'root'
-})
+} )
 export class BreakpointService {
   private _unsubscribe$ = new Subject();
+  private _breakpoints: string[] = [];
 
-  constructor(private _breakpointObserver: BreakpointObserver) { }
-  
-  breakpointSubscriber() {
-    this._breakpointObserver
-    .observe(Object.values(Breakpoints)).pipe(
-      takeUntil( this._unsubscribe$ )
-    ).subscribe(result => {
-      if (result.matches) {
-        this.activateHandsetLayout(result);
-      }
-    });
+  constructor(
+    private _breakpointObserver: BreakpointObserver
+    , private _platform: Platform ) { }
+
+  get isBrowser(): boolean {
+    return this._platform.isBrowser && !this._platform.ANDROID && !this._platform.IOS;
   }
 
-  breakpointUnsubscriber() {
+  get isMobile(): boolean {
+    return this._platform.ANDROID || this._platform.IOS;
+  }
+
+  get breakpoints(): string[] {
+    this._breakpoints = [];
+    for ( let key in Breakpoints ) {
+      if ( ( this.isBrowser && Breakpoints[key].indexOf( 'orientation' ) === -1 )
+        || ( !this.isBrowser && Breakpoints[key].indexOf( 'orientation' ) !== -1 ) ) {
+        this._breakpoints.push( Breakpoints[key] );
+      }
+    }
+    return this._breakpoints;
+  }
+
+  breakpointSubscriber(): void {
+    this._breakpointObserver.observe( this.breakpoints ).pipe(
+      takeUntil( this._unsubscribe$ )
+    ).subscribe( result => {
+      if ( result.matches ) {
+        this.activateBreakpoints( result );
+      }
+    } );
+    console.log( this._platform );
+  }
+
+  breakpointUnsubscriber(): void {
     this._unsubscribe$.next();
     this._unsubscribe$.complete();
   }
-  
-  activateHandsetLayout(result) {
-    console.log(Breakpoints, result);
+
+  activateBreakpoints( result ): void {
+    console.log( Breakpoints, result );
   }
 }
