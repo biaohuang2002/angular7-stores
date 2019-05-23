21bcc97683dcc7a6864a0539192fc94e90ebf38d
Biao Huang <biaohuang2002@gmail.com>
2019-05-23 10:42:18 +0200

    angular 7 scss configuration

--- projects/angular7-stores/src/app/products/product-list/product-list.component.ts
+++ projects/angular7-stores/src/app/products/product-list/product-list.component.ts
@@ -7,7 +7,7 @@ import { throwError } from 'rxjs';
 @Component( {
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.scss']
 } )
 export class ProductListComponent implements OnInit {
   productListings;
